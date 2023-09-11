166c942bafe3119e93f2699b50d95ca1addb88a1
Analimalu <124834271+Analimalu@users.noreply.github.com>
2023-09-11 18:17:08 -0300

    Create body.java

---
+++ body.java
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>meu programa em Js</title>
+</head>
+<body>
+    <h1>Minha pagina em js</h1>
+    <script>
+        window.alert('Boa tarde, queridos')
+        window.alert('oiiii')
+
+        var nome=window.prompt('Digite seu nome')
+        document.write('<br>', nome)
+
+        var cidade=window.prompt('Digite sua cidade')
+        window.alert(nome)
+        document.write('<br>',cidade)
+
+        var endereço=window.prompt('Digite seu endereço')
+        window.alert(nome)
+        
+
+    </script>
+    
+</body>
+</html>
